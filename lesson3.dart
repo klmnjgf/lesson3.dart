7f0fe62c2800ac344d01524c7aaeb287c58f57c3
klmnjgf <abek07898@gmail.com>
2023-10-09 18:25:39 +0600

    first commit

---
+++ lesson3.dart
@@ -0,0 +1,55 @@
+void main(List<String> args) {
+  Map userInfo1 = {
+    "name": "Aisuluu",
+    "surname": 'Beknazarova',
+    'age': 20,
+    "isMan": false
+  };
+  print(userInfo1);
+  print(userInfo1["age"]);
+
+  Map userInfo2 = {
+    "name": "Eugene",
+    "surname": 'Tyan',
+    'age': 30,
+    "isMan": true
+  };
+  print(userInfo2);
+  print(userInfo2["age"]);
+
+  Map userInfo3 = {
+    "name": "Asel",
+    "surname": 'Boldubekova',
+    'age': 41,
+    "isMan": false
+  };
+  print(userInfo3);
+  print(userInfo3["age"]);
+
+  Map userInfo4 = {
+    "name": "Umar",
+    "surname": 'Beknazarov',
+    'age': 22,
+    "isMan": true
+  };
+  print(userInfo4);
+  print(userInfo4["age"]);
+
+  Map userInfo5 = {
+    "name": "Elina",
+    "surname": 'Orozova',
+    'age': 27,
+    "isMan": false
+  };
+  print(userInfo5);
+  print(userInfo5["age"]);
+
+  var test = 12;
+  test = 22;
+  test = 32;
+  test = 42;
+  test = 52;
+
+  final useName; 
+  userName1 = Aisuluu
+}
